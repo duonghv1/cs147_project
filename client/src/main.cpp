51f189d021aeddb2baa4dbf2ead141d591772ffe
YRTang <SarahTYR09@outlook.com>
2023-06-05 16:21:18 -0700

    integrate microphone part in main.cpp

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -4,83 +4,41 @@
 #include <WiFi.h>
 #include <inttypes.h>
 #include <stdio.h>
-#include "esp_system.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "nvs.h"
-#include "nvs_flash.h"
 
 #include <Adafruit_AHTX0.h>
 #include "light.h"
-
+#include "microphone.h"
 
 #define CALIBRATION_ON false
 #define LIGHT_SENSOR 33
+#define MIC_SENSOR 15
+
+#define DEBUG_MODE true
 
 
 Adafruit_AHTX0 aht;
 
-char ssid[50] = "UCInet Mobile Access";//"Gone with the Wind"; // your network SSID (name)
+char ssid[50] = "UCInet Mobile Access"; //"Gone with the Wind";// your network SSID (name)
 char pass[50] = {0}; //"zotzotzot"; // your network password (use for WPA, or use as key for WEP)
 
-
+String public_IP = "54.177.115.132";//"18.219.240.227";
+const int port = 5000;
 // Number of milliseconds to wait without receiving any data before we give up
 const int kNetworkTimeout = 30 * 1000;
 // Number of milliseconds to wait if no data is available before trying again
 const int kNetworkDelay = 1000;
-// static int LIGHT_SENSOR = 33;
+
+const int uploadFreq = 20*1000; //20 sec per update to the server
+const int samplingFreq = 5*1000; //5 sec => for each sensor
+unsigned long upload_timer;
 
 String var;
 sensors_event_t humidity, temp;
 uint16_t light_read;
 double light_val;
-String public_IP = "54.177.115.132";//"18.219.240.227";
+int sound_val = 0;
 
-void nvs_access() {
-  // Initialize NVS
-  esp_err_t err = nvs_flash_init();
-  if (err == ESP_ERR_NVS_NO_FREE_PAGES ||
-    err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-    // NVS partition was truncated and needs to be erased
-    // Retry nvs_flash_init
-    ESP_ERROR_CHECK(nvs_flash_erase());
-    err = nvs_flash_init();
-  }
-  ESP_ERROR_CHECK(err);
-
-  // Open
-  Serial.printf("\n");
-  Serial.printf("Opening Non-Volatile Storage (NVS) handle... ");
-  nvs_handle_t my_handle;
-  err = nvs_open("storage", NVS_READWRITE, &my_handle);
-  if (err != ESP_OK) {
-    Serial.printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
-  } else {
-    Serial.printf("Done\n");
-    Serial.printf("Retrieving SSID/PASSWD\n");
-    size_t ssid_len;
-    size_t pass_len;
-    err = nvs_get_str(my_handle, "ssid", ssid, &ssid_len);
-    err |= nvs_get_str(my_handle, "pass", pass, &pass_len);
-  switch (err) {
-    case ESP_OK:
-      Serial.printf("Done\n");
-      //Serial.printf("SSID = %s\n", ssid);
-      //Serial.printf("PASSWD = %s\n", pass);
-      break;
-    case ESP_ERR_NVS_NOT_FOUND:
-      Serial.printf("The value is not initialized yet!\n");
-      break;
-    default:
-      Serial.printf("Error (%s) reading!\n", esp_err_to_name(err));
-    }
-  }
-
-  // Close
-  nvs_close(my_handle);
-}
-
-void sensor_configuration(){
+void AHT_setup(){
   if (! aht.begin()) {
     Serial.println("Could not find AHT? Check wiring");
     while (1) delay(10);
@@ -88,7 +46,7 @@ void sensor_configuration(){
   // Serial.println("AHT10 or AHT20 found");
 }
 
-void setup() {
+void wifi_setup() {
   Serial.begin(9600);
   delay(1000);
   // Retrieve SSID/PASSWD from flash before anything else
@@ -115,11 +73,20 @@ void setup() {
   Serial.println(WiFi.localIP());
   Serial.println("MAC address: ");
   Serial.println(WiFi.macAddress());
+  Serial.println();
+}
 
-  sensor_configuration();
+void setup(){
+  wifi_setup();
+
+  AHT_setup();
   if (CALIBRATION_ON){
     light_calibration(LIGHT_SENSOR);
   }
+
+  //microphone setup
+  pinMode(MIC_SENSOR, INPUT);
+  upload_timer = millis() + uploadFreq;
 }
 
 void loop() {
@@ -130,69 +97,83 @@ void loop() {
   light_read = analogRead(LIGHT_SENSOR);
   light_val = map(light_read);
 
+  // Read Sound Data
+  sound_val += digitalRead(MIC_SENSOR);
+  
+  // PRINT RESULTS
   Serial.print("Temperature: "); Serial.print(temp.temperature); Serial.println(" degrees C");
   Serial.print("Humidity: "); Serial.print(humidity.relative_humidity); Serial.println("% rH");
-  Serial.print("Light Level: "); Serial.print(light_val*100, 3); Serial.println("%");
+  Serial.print("Light Level: "); Serial.print(light_val*100, 2); Serial.println("%");
+  Serial.print("Sound Exists: "); Serial.println(sound_val);
 
-  // Print to the Webpage
-  int err = 0;
-  WiFiClient c;
-  HttpClient http(c);
+  // Upload data to the server
+  // Question: 
+  //    Is MIC connected to the same board as other sensors?
+  //       if yes => Temp & Light sensor only need to sample at the same rate as uploading rate
+  //       if no => the ESP32 connected with Temp&Light can implement sleeping mode; and the data collecting format(var=?) needs to be changed
+  //        ==> we can add a name to each parameter such as "temp=None,light=None,mic=3"
+  if (!DEBUG_MODE){
+    if (millis() > upload_timer){
+      int err = 0;
+      WiFiClient c;
+      HttpClient http(c);
 
-  var = (String)"/?var=" + (String)temp.temperature + "," + (String)humidity.relative_humidity;
-  var += "," + (String)light_val;
+      var = (String)"/?var=" + (String)temp.temperature + "," + (String)humidity.relative_humidity;
+      var += "," + (String)light_val;
+      var += "," + (String)sound_val;
 
-  err = http.get(public_IP.c_str(), 5000, var.c_str());
-  // err = http.get("127.0.0.1", 5000, var.c_str());
-  
-  if (err == 0) {
-    // Serial.println("\nstartedRequest ok");
-    err = http.responseStatusCode();
-    if (err >= 0) {
-      // Serial.print("Got status code: ");
-      // Serial.println(err);
+      err = http.get(public_IP.c_str(), port, var.c_str());
+      sound_val = 0;
+    
+      if (err == 0) {
+        // Serial.println("\nstartedRequest ok");
+        err = http.responseStatusCode();
+        if (err >= 0) {
+          // Serial.print("Got status code: ");
+          // Serial.println(err);
 
-      err = http.skipResponseHeaders();
-      if (err >= 0) {
-        int bodyLen = http.contentLength();
-        // Serial.print("Content length is: ");
-        // Serial.println(bodyLen);
-        // Serial.println();
-        // Serial.println("Body returned follows:");
+          err = http.skipResponseHeaders();
+          if (err >= 0) {
+            int bodyLen = http.contentLength();
+            // Serial.print("Content length is: ");
+            // Serial.println(bodyLen);
+            // Serial.println();
+            // Serial.println("Body returned follows:");
 
-        // Now we've got to the body, so we can print it out
-        unsigned long timeoutStart = millis();
-        char c;
-        // Whilst we haven't timed out & haven't reached the end of the body
-        while ((http.connected() || http.available()) && ((millis() - timeoutStart) < kNetworkTimeout)) {
-          if (http.available()) {
-            c = http.read();
-            // Print out this character
-            // Serial.print(c);
-            
-            bodyLen--;
-            // We read something, reset the timeout counter
-            timeoutStart = millis();
+            // Now we've got to the body, so we can print it out
+            unsigned long timeoutStart = millis();
+            char c;
+            // Whilst we haven't timed out & haven't reached the end of the body
+            while ((http.connected() || http.available()) && ((millis() - timeoutStart) < kNetworkTimeout)) {
+              if (http.available()) {
+                c = http.read();
+                // Print out this character
+                // Serial.print(c);
+                
+                bodyLen--;
+                // We read something, reset the timeout counter
+                timeoutStart = millis();
+              } else {
+                // We haven't got any data, so let's pause to allow some to arrive
+                delay(kNetworkDelay);
+              }
+            }
           } else {
-            // We haven't got any data, so let's pause to allow some to arrive
-            delay(kNetworkDelay);
+            Serial.print("Failed to skip response headers: ");
+            Serial.println(err);
           }
+        } else {
+          Serial.print("Getting response failed: ");
+          Serial.println(err);
         }
       } else {
-        Serial.print("Failed to skip response headers: ");
+        Serial.print("Connect failed: ");
         Serial.println(err);
       }
-    } else {
-      Serial.print("Getting response failed: ");
-      Serial.println(err);
+      http.stop();
     }
-  } else {
-    Serial.print("Connect failed: ");
-    Serial.println(err);
+    // End of Upload
   }
-  http.stop();
-  
   Serial.println();
-  // And just stop, now that we've tried a download
-  delay(10000);
+  delay(samplingFreq);
 }
---
+++ client/src/microphone.h
@@ -0,0 +1,7 @@
+// microphone.h
+#ifndef MICROPHONE_H
+#define MICROPHONE_H
+
+// Function declaration(s) and other content
+
+#endif // MICROPHONE_H
